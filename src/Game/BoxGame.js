f05855b31b34ad876e2da4df12f7291d6aec9efa
Xondamir <xondamirxudayorov@gmail.com>
2022-04-13 16:24:27 +0500

    second commit

--- src/Game/BoxGame.js
+++ src/Game/BoxGame.js
@@ -1,14 +1,47 @@
 import react from "react";
 import "./style/BoxGame.css";
+
+var colors = ["white", "black", "red", "green", "blue", "yellow"];
 class BoxGame extends react.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      colored_boxes: [
+        {
+          id: Math.floor(Math.random() * 81),
+          color: colors[Math.floor(Math.random() * colors.length)],
+        },
+        {
+          id: Math.floor(Math.random() * 81),
+          color: colors[Math.floor(Math.random() * colors.length)],
+        },
+        {
+          id: Math.floor(Math.random() * 81),
+          color: colors[Math.floor(Math.random() * colors.length)],
+        },
+        {
+          id: Math.floor(Math.random() * 81),
+          color: colors[Math.floor(Math.random() * colors.length)],
+        },
+        {
+          id: Math.floor(Math.random() * 81),
+          color: colors[Math.floor(Math.random() * colors.length)],
+        },
+      ],
+    };
   }
 
   render() {
     let boxes = [];
     for (let i = 0; i < 81; i++) {
-      boxes.push(<Box id={i} color={"default"} />);
+      let box = this.state.colored_boxes.filter((colored_box, j) => {
+        return colored_box.id == i + 1;
+      })[0];
+      if (box) {
+        boxes.push(<Box id={box.id} color={box.color} />);
+      } else {
+        boxes.push(<Box id={i + 1} color="default" />);
+      }
     }
     return (
       <div className="box-game-container">
